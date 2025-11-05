2a6a1373c1e4ed10e5328f673ba846edec137d57
Nahuel Francia <155550180+Nawe1337@users.noreply.github.com>
2025-11-04 21:07:40 -0300

    Cambios en target

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://backend-mongodb-atlas.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
